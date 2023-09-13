b54f98078dd58c1e1b839035a68bcc318b3a320c
Danilo Arioli <130993253+danilo-arioli@users.noreply.github.com>
2023-09-13 17:32:08 +0200

    transferred telescope keymaps to telescope configsi, added some general keymaps bindings

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,13 @@
+-- Move your lines when selected in visual mode
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- Keep your cursor at the beginning of the line when alligning lines with J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep your cursor in position when scrolling through the page
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- use leader-P to preserve previous copy when replacing something
+vim.keymap.set("x", "<leader>p", [["_dP]])
