cf016e65f9b534d721a1375c132e7bbf274ab2ed
Danilo Arioli <130993253+danilo-arioli@users.noreply.github.com>
2023-09-13 15:46:26 +0200

    first commit

---
+++ lua/core/options.lua
@@ -0,0 +1,30 @@
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.wo.fillchars='eob: '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.autoread = true
+vim.opt.wrap = true
+vim.opt.wrapmargin = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.o.hlsearch = false
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
---
+++ lua/keymaps/neotree.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<C-e>', '<Cmd>Neotree focus<CR>')
