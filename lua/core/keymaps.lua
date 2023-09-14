fc5516bffef3b051c5d9bb25a105c1fdc12b215c
Danilo Arioli <130993253+danilo-arioli@users.noreply.github.com>
2023-09-14 17:36:58 +0200

    added toggle term and new keybindings for split and tabs

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,30 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- use leader-P to preserve previous copy when replacing something
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Open a new vertical split
+vim.api.nvim_set_keymap("n", "<leader>ns", ":vsp<CR>", { noremap = true, silent = true })
+
+-- Close the current split
+vim.api.nvim_set_keymap("n", "<leader>cs", ":q<CR>", { noremap = true, silent = true })
+
+-- Navigate between splits
+vim.api.nvim_set_keymap("n", "<leader>sh", ":wincmd h<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>sj", ":wincmd j<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>sk", ":wincmd k<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>sl", ":wincmd l<CR>", { noremap = true, silent = true })
+
+-- Alternate between splits
+vim.api.nvim_set_keymap("n", "<leader><Tab>", "<C-w>w", { noremap = true, silent = true })
+
+-- Open a new tab
+vim.api.nvim_set_keymap("n", "<leader>nt", ":tabnew<CR>", { noremap = true, silent = true })
+
+-- Close the current tab
+vim.api.nvim_set_keymap("n", "<leader>ct", ":tabclose<CR>", { noremap = true, silent = true })
+
+-- Go to the next tab
+vim.api.nvim_set_keymap("n", "<Tab>", ":tabnext<CR>", { noremap = true, silent = true })
+
+-- Go to the previous tab
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":tabprevious<CR>", { noremap = true, silent = true })
