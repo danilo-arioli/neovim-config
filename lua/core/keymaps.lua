c82b81ebc5e7848cc79e4433274eb1258988ce9b
Danilo Arioli <130993253+danilo-arioli@users.noreply.github.com>
2023-09-18 09:56:42 +0200

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,18 +1,18 @@
 -- Map save to ctrl + s
 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 -- Move your lines when selected in visual mode
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv",{ noremap = true, silent = true })
 
 -- Keep your cursor at the beginning of the line when alligning lines with J
-vim.keymap.set("n", "J", "mzJ`z")
+vim.api.nvim_set_keymap("n", "J", "mzJ`z",{ noremap = true, silent = true })
 
 -- Keep your cursor in position when scrolling through the page
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz",{ noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz",{ noremap = true, silent = true })
 
 -- use leader-P to preserve previous copy when replacing something
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.api.nvim_set_keymap("x", "<leader>p", [["_dP]],{ noremap = true, silent = true })
 
 -- Open a new vertical split
 vim.api.nvim_set_keymap("n", "<leader>ns", ":vsp<CR>", { noremap = true, silent = true })
@@ -40,3 +40,4 @@ vim.api.nvim_set_keymap("n", "<Tab>", ":tabnext<CR>", { noremap = true, silent =
 
 -- Go to the previous tab
 vim.api.nvim_set_keymap("n", "<S-Tab>", ":tabprevious<CR>", { noremap = true, silent = true })
+
