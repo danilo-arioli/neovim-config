d461fb3f5a8d58b2a4992c960dba6e258d7c3fa5
Danilo Arioli <danilo_arioli@pop-os.localdomain>
2023-09-19 19:47:17 +0200

    commented out formatters_init because it does not work

--- lua/formatters_init.lua
+++ lua/formatters_init.lua
@@ -14,12 +14,12 @@ local formatters = {
 }
 
 
--- Check and install each formatter
-for _, formatter in ipairs(formatters) do
-	local binary_exists = vim.fn.executable(formatter.binary)
-	if binary_exists == 0 then
-		vim.api.nvim_out_write(formatter.name .. " is not installed. Installing...\n")
-		vim.fn.system("MasonInstall " .. formatter.name)
-		vim.api.nvim_out_write(formatter.name .. " installed successfully.\n")
-	end
-end
+-- -- Check and install each formatter
+--                    for _, formatter in ipairs(formatters) do
+-- 	local binary_exists = vim.fn.executable(formatter.binary)
+-- 	if binary_exists == 0 then
+-- 		vim.api.nvim_out_write(formatter.name .. " is not installed. Installing...\n")
+-- 		vim.fn.system("MasonInstall " .. formatter.name)
+-- 		vim.api.nvim_out_write(formatter.name .. " installed successfully.\n")
+-- 	end
+-- end
