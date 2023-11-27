fa4e4547a13c0c7012793ab9a1840a82061f4de7
Danilo Arioli <danilo.arioli@iMac-di-agenziamete.local>
2023-11-27 18:18:16 +0100

    added automatic installation for mason formatters

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,7 +13,41 @@ return {
 	{
 		"williamboman/mason.nvim",
 		lazy = false,
-		config = true,
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			-- import mason
+			local mason = require("mason")
+
+			-- import mason-lspconfig
+			local mason_lspconfig = require("mason-lspconfig")
+
+			local mason_tool_installer = require("mason-tool-installer")
+
+			-- enable mason and configure icons
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"prettier", -- prettier formatter
+					"stylua", -- lua formatter
+					"eslint_d", -- js linter
+					"pretty-php", -- php formatter
+					"php-cs-fixer", -- php formatter
+					"phpcbf", -- php formatter
+				},
+			})
+		end,
 	},
 
 	-- LSP
